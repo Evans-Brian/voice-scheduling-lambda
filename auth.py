940e1aed55d866c7dabfa6571167bc4474d7c182
Evans-Brian <bevs137@gmail.com>
2025-02-06 21:11:07 -0500

    revert auth changes - unneeeded

--- auth.py
+++ auth.py
@@ -12,31 +12,18 @@ def get_credentials():
         Credentials, the obtained credential.
     """
     creds = None
-    token_path = 'token.pickle'
-    
-    # If running in Lambda, use /tmp directory
-    if os.getenv('AWS_LAMBDA_FUNCTION_NAME'):
-        token_path = '/tmp/token.pickle'
-        # Copy token.pickle to /tmp if it exists in the package
-        if os.path.exists('token.pickle') and not os.path.exists(token_path):
-            with open('token.pickle', 'rb') as src, open(token_path, 'wb') as dst:
-                dst.write(src.read())
-    
-    if os.path.exists(token_path):
-        with open(token_path, 'rb') as token:
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
     
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-            # Save the refreshed credentials
-            with open(token_path, 'wb') as token:
-                pickle.dump(creds, token)
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', GOOGLE_CALENDAR_SCOPES)
             creds = flow.run_local_server(port=0)
-            with open(token_path, 'wb') as token:
-                pickle.dump(creds, token)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
     
     return creds 
